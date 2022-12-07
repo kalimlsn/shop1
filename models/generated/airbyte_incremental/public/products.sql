3869c91cf7e760dd11c8da705ba368cf9f2123d5
kalimlsn <kalimlsn@users.noreply.github.com>
2022-12-07 11:47:34 +0530

    Update products.sql

--- models/generated/airbyte_incremental/public/products.sql
+++ models/generated/airbyte_incremental/public/products.sql
@@ -8,7 +8,7 @@
 -- depends_on: {{ ref('products_scd') }}
 select
     _airbyte_unique_key,
-    {{ adapter.quote('id') }} as test,
+    {{ adapter.quote('id') }} ,
     title,
     created_at as date_created,
     updated_at as date_modified,
